84cc42deb97a1a902a6e51383d183446c25bf554
AiMlxV <41025@chs.ac.th>
2025-09-16 02:08:11 +0700

    fix: change the path

--- back/server.js
+++ back/server.js
@@ -15,7 +15,7 @@ const __dirname = dirname(__filename);
 const app = express();
 
 app.use(express.json());
-app.use(express.static(join(__dirname, '../front')));
+app.use(express.static(join(__dirname, '../frontend')));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
